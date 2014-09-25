b9bf213bba498646de26aad12f01da2416f5b9c3
sakuma <sakuma.idac@gmail.com>
2014-09-25 11:04:15 +0000

    first commit

---
+++ Config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = [
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'sample',
+		'prefix' => '',
+	];
+}
